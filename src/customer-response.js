770c36810f471c68f3b2de8d41daeaf990cf5a25
Abdul-rashed <122110062+Abdul-Rashed@users.noreply.github.com>
2023-03-13 20:55:37 +0530

    added lambdalayer

--- src/customer-response.js
+++ src/customer-response.js
@@ -1,46 +1,55 @@
 const AWS = require('aws-sdk');
 const Joi = require('joi');
+const dynamo = new AWS.DynamoDB({ apiVersion: "2012-08-10", region: 'us-east-1' });
+const axios = require("axios");
+const { create, convert } = require('xmlbuilder2');
+
+const schema = Joi.object({
+    documentUploadRequest: Joi.object({
+        housebill: Joi.number().integer(),
+        fileNumber: Joi.number().integer(),
+        contentType: Joi.any().required(),
+        docType: Joi.string().valid(
+            "CERTIFICAT",
+            "CONSULAR",
+            "CUST RATE",
+            "CUSTOMS",
+            "DANGEROUS",
+            "DCCL",
+            "DECON",
+            "HCPOD",
+            "IBU",
+            "IMPORT LIC",
+            "INSURANCE",
+            "INVOICE",
+            "MSDS",
+            "OCCL",
+            "OMNI RA",
+            "ORIG BOL",
+            "PACKING",
+            "PO",
+            "POD",
+            "PRO FORMA",
+            "RA",
+            "SED",
+            "SLI",
+            "WAYBILL"
+        )
+            .required(),
+        b64str: Joi.string().required()
+    }).or("housebill", "fileNumber")
+        .required()
+}).required();
 
 module.exports.handler = async (event) => {
+    console.log("Event",event)
     const payload = event.body;
-    console.log(event)
-    const schema = Joi.object({
-        documentUploadRequest: Joi.object({
-            housebill: Joi.number().integer(),
-            fileNumber: Joi.number().integer(),
-            contentType: Joi.any().required(),
-            docType: Joi.string().valid(
-                "CERTIFICAT",
-                "CONSULAR",
-                "CUST RATE",
-                "CUSTOMS",
-                "DANGEROUS",
-                "DCCL",
-                "DECON",
-                "HCPOD",
-                "IBU",
-                "IMPORT LIC",
-                "INSURANCE",
-                "INVOICE",
-                "MSDS",
-                "OCCL",
-                "OMNI RA",
-                "ORIG BOL",
-                "PACKING",
-                "PO",
-                "POD",
-                "PRO FORMA",
-                "RA",
-                "SED",
-                "SLI",
-                "WAYBILL"
-            )
-                .required(),
-            b64str: Joi.string().required()
-        }).or("housebill", "fileNumber")
-            .required()
-    }).required();
-
+    const data =payload.documentUploadRequest
+    console.log(data.housebill)
+    const housebill=data.housebill
+    console.log("housebill",housebill)
+   
+   
     const { error } = schema.validate(payload);
     if (error) {
         return {
@@ -50,6 +59,40 @@ module.exports.handler = async (event) => {
             })
         };
     }
+
+    const customer_id = event.enhancedAuthContext.customerId;
+    console.log("customer_id",customer_id)
+
+    const isIvia = customer_id === process.env.IVIA_CUSTOMERID;
+
+    // If customerId is not Ivia, check housebill entitlements
+    if (!isIvia) {
+        const entitlementParams = {
+          TableName: process.env.CUSTOMER_ENTITLEMENT_TABLE,
+          FilterExpression: 'CustomerID = :customerId and HouseBillNumber = :housebill',
+          ExpressionAttributeValues: {
+            ':customerId': { S: customer_id },
+            ':housebill': { S: housebill }
+          }
+        };
+        console.log("entitlementParams", entitlementParams)
+        let entitlementResult;
+        try {
+          entitlementResult = await dynamo.scan(entitlementParams).promise();
+          console.log("entitlementResult", entitlementResult)
+        } catch (err) {
+          console.log("Error", err);
+          return { statusCode: 402, body: 'Housebill not found' };
+        }
+        console.log(entitlementResult.Items[0])
+        console.log(!entitlementResult.Items[0])
+        if (!entitlementResult.Items[0]) {
+          return { statusCode: 403, body: 'Housebill is incorrect' };
+        }
+      }
+
+      await fetchPkOrderNumberByHousebillNumber(data);
+
     return {
         statusCode: 200,
         body: JSON.stringify({
@@ -57,3 +100,139 @@ module.exports.handler = async (event) => {
         })
     };
 }; 
+
+
+async function fetchPkOrderNumberByHousebillNumber(data) {
+    const housebill=data.housebill;
+    const params = {
+        TableName: process.env.SHIPMENT_HEADER_TABLE,
+        IndexName: process.env.SHIPMENT_HEADER_TABLE_INDEX,
+        KeyConditionExpression: 'Housebill = :housebill',
+        ExpressionAttributeValues: {
+            ':housebill': { S: housebill }
+        },
+        ProjectionExpression: 'PK_OrderNo'
+    };
+    console.log("params", params)
+    try {
+        const queryShipmentHeader = await dynamo.query(params).promise();
+        console.log("queryShipmentHeader", queryShipmentHeader.Items[0].PK_OrderNo.S)
+        let PK_OrderNo = queryShipmentHeader.Items[0].PK_OrderNo.S;
+        console.log("PK_OrderNo", PK_OrderNo)
+        await validateAddressMapping(PK_OrderNo,data);
+        console.log("data",data)
+        
+    } catch (err) {
+        console.error('Error fetching data from DynamoDB', err);
+        throw err;
+    }
+}
+
+
+
+async function validateAddressMapping(PK_OrderNo,data) {
+    try {
+
+        // query the omni-wt-address-mapping-dev table
+        const params = {
+            TableName: process.env.ADDRESS_MAPPING_TABLE,
+            KeyConditionExpression: 'FK_OrderNo = :o',
+            ExpressionAttributeValues: {
+                ':o': { S: PK_OrderNo }
+            },
+        };
+        const queryMapping = await dynamo.query(params).promise();
+        console.log("data.Items.length", queryMapping.Items.length)
+        // check if the cc_con_zip and cc_con_address values are valid
+        if (queryMapping.Items.length > 0) {
+            const item = queryMapping.Items[0];
+            console.log("item", item)
+            console.log(item.cc_con_zip)
+            console.log(item.cc_con_address)
+            if (item.cc_con_zip.S == 1 && item.cc_con_address.S == 1) {
+                console.log('cc_con_zip:', item.cc_con_zip);
+                console.log('cc_con_address:', item.cc_con_address);
+                await makeJsonToXml(data);
+
+            } else {
+                console.error('Invalid cc_con_zip or cc_con_address in omni-wt-address-mapping-dev table:', item);
+
+            }
+        } else {
+            console.error('No record found in omni-wt-address-mapping-dev table for FK_OrderNo:', PK_OrderNo);
+        }
+    } catch (err) {
+        console.error('Error fetching address mapping from omni-wt-address-mapping-dev table:', err);
+        throw err;
+    }
+}
+
+
+
+
+async function makeJsonToXml(data) {
+    console.log("data",data)
+    const date = new Date();
+    const timestamp = date.toISOString().replace(/[-:]/g, '').slice(0, 14);
+
+    let xml = "";
+    xml = convert({
+        "soap:Envelope": {
+            "@xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
+            "@xmlns:xsd": "http://www.w3.org/2001/XMLSchema",
+            "@xmlns:soap": "http://schemas.xmlsoap.org/soap/envelope/",
+            "soap:Body": {
+                AttachFileToShipment: {
+                    "@xmlns": "http://tempuri.org/",
+                    Housebill: data.housebill,
+                    Filename: data.housebill+'_'+data.docType+'_'+timestamp+'.pdf',
+                    DocType: data.docType,
+                    FileDataBase64: data.b64str
+                }
+            }
+        }
+    });
+
+    console.info("xml payload", xml);
+    await apiToPushObjectToWt(xml)
+
+}
+
+
+async function apiToPushObjectToWt(xml) {
+    const config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: process.env.UPLOAD_DOCUMENT_API,
+        headers: {
+            "content-type": "text/xml",
+        },
+        data: xml,
+    }
+    console.log("config", config)
+    try {
+        const axiosResponse = await axios(config); 
+
+        console.log('response', axiosResponse);
+
+        const statusCode = axiosResponse.status;
+        const message = axiosResponse.statusText;
+
+        const responseBody = await response(statusCode, message); 
+
+        console.log('Response body:', responseBody);
+    } catch (error) {
+        console.log('Error in API request:', error);
+    }
+
+}
+
+
+async function response(statusCode, message) {
+    const responseObj = {
+        httpStatus: statusCode,
+        message,
+    };
+
+    return JSON.stringify(responseObj);
+}
--- src/customer_authorization.js
+++ src/customer_authorization.js
@@ -1,143 +1,111 @@
 const AWS = require('aws-sdk');
 const dynamo = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
 const { dynamo_query } = require("./shared/dynamo");
-const axios = require("axios");
+
+
+const POLICY_ID = "bizCloud|a1b2";
+const INTERNALERRORMESSAGE = "Internal Error.";
 
 
 
-
-
-module.exports.handler = async (event, context) => {
-  console.log("Event",event)
-  const api_key = event.headers['x-api-key'];
-  const housebill = event.query.housebill;
-  console.log("apiKey", api_key)
-  console.log("housebill", housebill)
+module.exports.handler = async (event, context, callback) => {
   // validate the x-apiKEy from dynamoDB aas
-  let response;
   try {
-    response = await dynamo_query(
+    let api_key, params;
+    try {
+      console.info("Event: ", JSON.stringify(event));
+      api_key = event["headers"]["x-api-key"];
+    } catch (api_error) {
+      console.log("ApiKeyError", api_error);
+      return callback(response("400", "API Key not passed."));
+    }
+    const validation_response = validate_input(event.methodArn);
+    if (validation_response["status"] == "error") {
+      return callback(
+        null,
+        generate_policy(
+          POLICY_ID,
+          "Deny",
+          event.methodArn,
+          null,
+          validation_response.message
+        )
+      );
+    }
+    const response = await dynamo_query(
       process.env.TOKEN_VALIDATION_TABLE,
       process.env.TOKEN_VALIDATION_TABLE_INDEX,
       "ApiKey = :apikey",
       { ":apikey": { S: api_key } }
     );
     console.log("response", response)
-  } catch (err) {
-    console.log(err);
-    return { statusCode: 400, body: 'Unauthorized' };
-  }
-  console.log(response.Items)
+    console.log(response.Items)
 
-  console.log(!response.Items[0])
-  if (!response.Items[0]) {
-    return { statusCode: 401, body: 'Unauthorized' };
-  }
-
-  const customerId = response.Items[0].CustomerID.S;
-  console.log("customerId", customerId)
-
-  const isIvia = customerId === process.env.IVIA_CUSTOMERID;
-
-  // If customerId is not Ivia, check housebill entitlements
-  if (!isIvia) {
-    const entitlementParams = {
-      TableName: process.env.CUSTOMER_ENTITLEMENT_TABLE,
-      FilterExpression: 'CustomerID = :customerId and HouseBillNumber = :housebill',
-      ExpressionAttributeValues: {
-        ':customerId': { S: customerId },
-        ':housebill': { S: housebill }
-      }
-    };
-    console.log("entitlementParams",entitlementParams)
-    let entitlementResult;
-    try {
-      entitlementResult = await dynamo.scan(entitlementParams).promise();
-      console.log("entitlementResult",entitlementResult)
-    } catch (err) {
-      console.log("Error", err);
-      return { statusCode: 402, body: 'Housebill not found' };
+    console.log(!response.Items[0])
+    if (!response.Items[0]) {
+      return { statusCode: 401, body: 'Unauthorized' };
     }
-    console.log(entitlementResult.Items[0])
-    console.log(!entitlementResult.Items[0])
-    if (!entitlementResult.Items[0]) {
-      return { statusCode: 403, body: 'Housebill is incorrect' };
+
+    
+    console.log("customerId", customer_id)
+    if (event.methodArn.includes("/customer-response")) {
+      return callback(
+        null,
+        generate_policy(POLICY_ID, "Allow", event.methodArn, customer_id)
+      );
     }
+  } catch (error) {
+    console.log("error:handler", error);
+    return callback(response(500, INTERNALERRORMESSAGE));
   }
-  console.log( { statusCode: 200, body: 'Authorized' });
-  await fetchFkOrderNumberByHousebillNumber(housebill,event)
-
-
 };
 
 
-
-
-
-async function fetchFkOrderNumberByHousebillNumber(housebill,event) {
-  // const housebill= '6008067'
-  const params = {
-      // TableName: process.env.PKORDERNO_TABLE,
-      // IndexName: process.env.PKORDERNO_TABLE_INDEX,
-      TableName: 'omni-wt-rt-shipment-header-dev',
-      IndexName: 'Housebill-index',
-      KeyConditionExpression: 'Housebill = :housebill',
-      ExpressionAttributeValues: {
-          ':housebill': { S: housebill }
-        },
-      ProjectionExpression: 'PK_OrderNo'
-  };
-  console.log("params",params)
+const generate_policy = (
+  principal_id,
+  effect,
+  method_arn,
+  customer_id = null,
+  message = null
+) => {
   try {
-    const data = await dynamo.query(params).promise();
-    console.log("data",data.Items[0].PK_OrderNo.S)
-    let PK_OrderNo = data.Items[0].PK_OrderNo.S;
-    console.log("PK_OrderNo",PK_OrderNo)
-    await validateAddressMapping(PK_OrderNo,event);
-  } catch (err) {
-    console.error('Error fetching data from DynamoDB', err);
-    throw err;
-  }
-}
-
-
-
-
-
-async function validateAddressMapping(PK_OrderNo,event) {
-  try {
-
-    // query the omni-wt-address-mapping-dev table
-    const params = {
-      TableName: 'omni-wt-address-mapping-dev',
-      KeyConditionExpression: 'FK_OrderNo = :o',
-      ExpressionAttributeValues: {
-          ':o': { S: PK_OrderNo }
+    const policy = {};
+    policy.principalId = principal_id;
+    policy.policyDocument = {
+      Version: "2012-10-17",
+      Statement: [
+        {
+          Sid: "ApiAccess",
+          Action: "execute-api:Invoke",
+          Effect: effect,
+          Resource: method_arn,
         },
+      ],
     };
-    const data = await dynamo.query(params).promise();
-    console.log("data.Items.length",data.Items.length)
-    // check if the cc_con_zip and cc_con_address values are valid
-    if (data.Items.length > 0) {
-      const item = data.Items[0];
-      console.log("item",item)
-      console.log(item.cc_con_zip)
-      console.log(item.cc_con_address)
-      if (item.cc_con_zip.S == 1 && item.cc_con_address.S == 1) {
-        console.log('cc_con_zip:', item.cc_con_zip);
-        console.log('cc_con_address:', item.cc_con_address);
-        console.log("event.body",event.body)
-        
-      } else {
-        console.error('Invalid cc_con_zip or cc_con_address in omni-wt-address-mapping-dev table:', item);
-        
-      }
-    } else {
-      console.error('No record found in omni-wt-address-mapping-dev table for FK_OrderNo:', PK_OrderNo);
-      return false;
+    if (message) {
+      policy.context = { message };
+    } else if (customer_id) {
+      policy.context = { customerId: customer_id };
     }
-  } catch (err) {
-    console.error('Error fetching address mapping from omni-wt-address-mapping-dev table:', err);
-    throw err;
+    console.info("Policy:", JSON.stringify(policy));
+    return policy;
+  } catch (error) {
+    throw INTERNALERRORMESSAGE;
   }
-} 
+};
+
+
+const validate_input = (method_arn) => {
+  if (method_arn.includes("/shipment/addmilestone")) {
+    return { status: "success" };
+  } else {
+    return { status: "success" };
+  }
+};
+
+const response = (code, message) => {
+  return JSON.stringify({
+    httpStatus: code,
+    message,
+  });
+};
